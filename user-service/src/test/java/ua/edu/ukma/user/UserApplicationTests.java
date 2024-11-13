856fc1563be252e044ea7ffc79426bb5281d2a9f
Danylo Shlapak <geografy2314@gmail.com>
2024-11-13 22:46:14 +0200

    Init skeleton project

---
+++ user-service/src/test/java/ua/edu/ukma/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
