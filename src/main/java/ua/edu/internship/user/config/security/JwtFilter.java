94ed790903cef8623f0b9d4a96795c22fa1f9532
Danylo Shlapak <d.shlapak2314@gmail.com>
2025-01-22 12:52:16 +0200

    INT-107 Refactor filter

--- src/main/java/ua/edu/internship/user/config/security/JwtFilter.java
+++ src/main/java/ua/edu/internship/user/config/security/JwtFilter.java
@@ -18,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import ua.edu.internship.user.service.business.JwtService;
 import ua.edu.internship.user.service.utils.exceptions.InvalidTokenException;
+
 import java.io.IOException;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -43,22 +44,24 @@ public class JwtFilter extends OncePerRequestFilter {
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
         log.debug("Processing request on URI: {}", request.getRequestURI());
         String authHeader = request.getHeader(AUTH_HEADER);
-        if (authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
-            String token = authHeader.substring(BEARER_PREFIX.length());
-            try {
-                TokenData tokenData = jwtService.parseToken(token);
-                Set<GrantedAuthority> authorities = tokenData.getPermissions().stream()
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toSet());
-                SecurityContextHolder.getContext()
-                        .setAuthentication(new UsernamePasswordAuthenticationToken(tokenData, null, authorities));
-            } catch (MalformedJwtException e) {
-                exceptionResolver.resolveException(request, response, null, new InvalidTokenException("Invalid token"));
-                return;
-            } catch (ExpiredJwtException e) {
-                exceptionResolver.resolveException(request, response, null, new InvalidTokenException("Token is expired"));
-                return;
-            }
+        if (authHeader == null || !authHeader.startsWith(BEARER_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = authHeader.substring(BEARER_PREFIX.length());
+        try {
+            TokenData tokenData = jwtService.parseToken(token);
+            Set<GrantedAuthority> authorities = tokenData.getPermissions().stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+            SecurityContextHolder.getContext()
+                    .setAuthentication(new UsernamePasswordAuthenticationToken(tokenData, null, authorities));
+        } catch (MalformedJwtException e) {
+            exceptionResolver.resolveException(request, response, null, new InvalidTokenException("Invalid token"));
+            return;
+        } catch (ExpiredJwtException e) {
+            exceptionResolver.resolveException(request, response, null, new InvalidTokenException("Token is expired"));
+            return;
         }
         filterChain.doFilter(request, response);
     }
